2f0a01045134c29b84542f3996eeaee2c62c364f
Miguel <piprojectsms@gmail.com>
2020-05-30 18:39:55 +0200

    Replace Point by Twist

--- src/andrewbot_base_control.py
+++ src/andrewbot_base_control.py
@@ -2,96 +2,38 @@
 import rospy
 import math
 from andrewbot_msgs.msg import RobotCommand
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Twist
+from andrewbot_base_driver import AndrewbotBaseDriver
 
+robot_base_pub = rospy.Publisher('andrewbot/RobotCommand', RobotCommand, queue_size=10)
+robot_base = AndrewbotBaseDriver()
 robot_command = RobotCommand()
-robot_command.command = [0x0d]+[0x00]*17
 
-
-def calculate_quadrant_properties(x,y):
-    """
-    Analyze point and return quadrant, magnitude and direction
-    """
-    if x<0:
-        quadrant = 1 if y>=0 else 4
-    else:
-        quadrant = 2 if y>=0 else 3
-            
-    magnitude = math.sqrt(math.pow(x,2) + math.pow(y,2))
+def sent_robot_command(command):    
+    robot_command.command = command
+    robot_base_pub.publish(robot_command)
     
-    try:
-        direction = abs(math.atan(y/x))
-    except ZeroDivisionError:
-        direction = math.pi/2
-                
-    return magnitude, direction, quadrant
+def move_base(linear, angular):
+    command = robot_base.rotate_base(angular)
+    if command is not None: 
+        sent_robot_command(command)
 
-
-def translate_point_to_velocity_cmd(x, y, v_max=255, magnitude_max=1.0):
-    """
-    Given a Point translate to velocity command to the robot
-    """
-    magnitude, direction, quadrant = calculate_quadrant_properties(x,y)
-
-    if quadrant in [1, 4]:
-        cmd_left= min(magnitude_max, magnitude)*v_max
-        cmd_right = min(magnitude_max, magnitude)*v_max *math.sin(direction)
-
-    elif quadrant in [2, 3]:
-        cmd_left= min(magnitude_max, magnitude)*v_max*math.sin(direction)
-        cmd_right = min(magnitude_max, magnitude)*v_max 
-
-    else:
-        raise ValueError("Unknown quadrant: {}".format(quadrant))
-
-    if quadrant in [3,4]:
-        cmd_left=-cmd_left
-        cmd_right=-cmd_right
-
-    return cmd_left, cmd_right
-
+    command = robot_base.translate_base(linear)
+    if command is not None: 
+        sent_robot_command(command)
 
 def move_base_callback(base_movement):
-    cmd_left, cmd_right=translate_point_to_velocity_cmd(base_movement.x, base_movement.y)
-    
-    dir_left = 0x01
-    dir_right = 0x01
-    
-    if cmd_left < 0:
-        dir_left=0x02
-    
-    if cmd_right < 0:
-        dir_right=0x02
-
-    robot_command.command[1]=dir_left
-    robot_command.command[2]=dir_right
-    robot_command.command[3]=int(abs(cmd_left))
-    robot_command.command[4]=int(abs(cmd_right))
-
-def prepare_message():
-    checksum = 0
-    for v in robot_command.command[:16]:
-        checksum += v
-
-    robot_command.command[16]=(checksum >> 8) & 0xff
-    robot_command.command[17]= checksum & 0xff
+    move_base(base_movement.linear.x, base_movement.angular.z)
     
 
 if __name__ == '__main__':
-    
-    
-    
     try:
-        rospy.init_node('andrewbot_base_control', anonymous=True)
-        
-        rospy.Subscriber("andrewbot/BaseCommand", Point, move_base_callback)
-        base_move_pub = rospy.Publisher('andrewbot/RobotCommand', RobotCommand, queue_size=10)
-        
+        rospy.init_node('andrewbot_base_control', anonymous=True)    
+        rospy.Subscriber("andrewbot/BaseCommand", Twist, move_base_callback)
+    
         rate = rospy.Rate(100)  # 10 ms                                
         
         while not rospy.is_shutdown():
-            prepare_message()
-            base_move_pub.publish(robot_command)
             rate.sleep()
 
     except rospy.ROSInterruptException:
---
+++ src/andrewbot_base_driver.py
@@ -0,0 +1,52 @@
+from andrewbot_utils.arduino_command import create_empty_command, prepare_message
+
+
+class AndrewbotBaseDriver(object):
+    def __init__(self, max_velocity=255):
+        self.v_max = max_velocity
+        
+
+    def _move_base_command(self, cmd_left, cmd_right):
+        command = create_empty_command()
+
+        dir_left = 0x01
+        dir_right = 0x01
+
+        if cmd_left < 0:
+            dir_left=0x02
+
+        if cmd_right < 0:
+            dir_right=0x02
+
+        command[0]=0x0d
+        command[1]=dir_left
+        command[2]=dir_right
+        command[3]=int(abs(cmd_left))
+        command[4]=int(abs(cmd_right))   
+
+        command = prepare_message(command)
+        return command
+        
+    
+    def rotate_base(self, angular):
+        if abs(angular) > 0.0: # turn
+            cmd_left=  angular * self.v_max
+            cmd_right = angular * self.v_max
+            
+            if angular > 0: # Turn left
+                cmd_left = -cmd_left
+            else:           # Turn right
+                cmd_right= -cmd_right
+
+            return self._move_base_command(cmd_left, cmd_right)
+        return None
+    
+
+    def translate_base(self, linear):
+        if linear != 0.0:
+            cmd_left = linear * self.v_max
+            cmd_right= linear * self.v_max    
+            return self._move_base_command(cmd_left, cmd_right)
+        return None
+    
+    
